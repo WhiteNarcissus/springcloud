2fb1eca669bad1faf7df7a92ed931449b3833b6a
mjc <1119250897@qq.com>
2019-06-13 14:55:30 +0800

    1 先搞两个spring-boot 启动 项目 module  为 eureka-server。Active Profiles peer1 和peer2 2 将1 和 2 (注册中心) 分别启动
    3 将要注册的微服务 启动，修改配置项为：spring.application.name=hello-service
    eureka.client.serviceUrl.defaultZone=http://peer1:1111/eureka/,http://peer2:1112/eureka/

---
+++ eureka-server1/src/main/java/com/gzcb/EurekaServer1Application.java
@@ -0,0 +1,19 @@
+package com.gzcb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Created by gcb on 2019/6/13.
+ */
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaServer1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer1Application.class, args);
+    }
+
+}
